4b5e40e61d713db4a5ac53b67ecea88660b6be20
Alejandro Gil <alejandrogilcontreras94@gmail.com>
2015-04-14 12:31:33 +0200

    Cambio método Update combo1

--- Scripts/Enemigos/enemigoCombo1.cs
+++ Scripts/Enemigos/enemigoCombo1.cs
@@ -7,13 +7,22 @@ public class enemigoCombo1 : enemigoCombo {
 
 	// Public variable that contains the speed of the enemy
 	public int combo = 1;
+	public bool comp = false;
+	public bool comp2 = false;
 
 	void Update(){
 		//destruir combo1 con la letra Q + M + G (pulsadas)
-		if (Input.GetKey ("q"))
-		    if(Input.GetKey ("m"))
-				if(Input.GetKeyDown("g"))
-				Destroy (gameObject);
+
+		if (Input.GetKeyDown ("q")) 
+			comp = true;
+
+		if (comp == true && Input.GetKeyDown ("m")) 
+				comp2 = true;
+
+		if (comp == true && comp2 == true && Input.GetKeyDown ("g"))
+					Destroy (gameObject);
+		
+		 
 	}
 
 }
